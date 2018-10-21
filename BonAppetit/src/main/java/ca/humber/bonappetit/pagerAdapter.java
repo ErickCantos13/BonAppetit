8e995f87b5bd8ca8d6092a6101ff5b1702011a7e
ErickCantos13 <31775210+ErickCantos13@users.noreply.github.com>
2018-10-21 13:57:06 -0400

    update2

---
+++ BonAppetit/src/main/java/ca/humber/bonappetit/pagerAdapter.java
@@ -0,0 +1,34 @@
+package ca.humber.bonappetit;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class pagerAdapter extends FragmentPagerAdapter {
+
+    int numberOfTabs;
+
+    public pagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.numberOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                tab1 tab1 = new tab1();
+                return tab1;
+            case 1:
+                tab2 tab2 = new tab2();
+                return tab2;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numberOfTabs;
+    }
+}
